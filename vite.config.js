a4dea52c54c1228b282400d8b4cdaedd9c535e9e
Graham4914 <67054735+Graham4914@users.noreply.github.com>
2025-02-02 14:50:03 +0800

    removered terser from vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   build: {
-    minify: 'terser',  // Enables JS minification
     sourcemap: mode === 'development',  // Only generate sourcemaps in dev
     rollupOptions: {
       external: mode === 'production' ? ['react-devtools'] : [],
